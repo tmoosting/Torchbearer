6352c24b75a0323d54a0cbe121f7dc29033d4735
worldsmithdev <worldsmithdev@gmail.com>
2019-11-08 17:14:37 +0100

    setup

---
+++ Assets/Scripts/Overworld/Tower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    public Sprite towerLo;
+    public Sprite towerHi;
+    [HideInInspector]
+    public bool explorable = false;
+    [HideInInspector]
+    public int towerID;
+
+    private void OnMouseOver()
+    {
+        if (explorable == true)
+        gameObject.GetComponent<SpriteRenderer>().sprite = towerHi;
+    }
+    private void OnMouseExit()
+    {
+        if (explorable == true)
+            gameObject.GetComponent<SpriteRenderer>().sprite = towerLo;
+    }
+    private void OnMouseDown()
+    {
+        if (explorable == true)
+        {
+            UIController.Instance.overworldInterface.ClickTower(this);
+        }
+
+    }
+}
