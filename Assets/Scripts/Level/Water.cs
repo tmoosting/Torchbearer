a02bf778d61c68dabea8f396c5387030def7a309
Pyrogalactic <52071312+Pyrogalactic@users.noreply.github.com>
2019-12-18 14:00:47 +0100

    Added water splash sound when entering water in forest scene

---
+++ Assets/Scripts/Level/Water.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    public AudioSource waterSource;
+    private void OnTriggerEnter2D(Collider2D other) //Checks for collisions with other colliders
+    {
+        if (other.gameObject.CompareTag("Player"))//is the colliding object the player?
+        {
+            waterSource.Play();
+        }
+    }
+}
